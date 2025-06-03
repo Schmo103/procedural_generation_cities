dc49cd42f3663be7ff63db66e875a578b56bb472
Drew M <yellow_six@icloud.com>
2025-06-03 08:11:33 -0500

    initializing project

---
+++ scripts/player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+@onready var head = $head
+@onready var standing_collision_shape = $standing_collision_shape
+@onready var crouching_collision_shape = $crouching_collision_shape2
+
+var current_speed = 5.0
+
+const walking_speed = 5.0
+const sprinting_speed = 8.0
+const crouching_speed = 3.0
+
+var jump_velocity = 4.5
+
+const mouse_sens = 0.25
+
+var lerp_speed = 10.0
+
+var direction = Vector3.ZERO
+
+var crouching_depth = -0.5
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-89), deg_to_rad(89))
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y,1.8 + crouching_depth,delta*lerp_speed)
+		standing_collision_shape.disabled = true
+		crouching_collision_shape.disabled = false
+	else:
+		standing_collision_shape.disabled = false
+		crouching_collision_shape.disabled = true
+		head.position.y = lerp(head.position.y,1.8,delta*lerp_speed)
+		
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprinting_speed
+		else:
+			current_speed = walking_speed
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_velocity
+		
+		var input_dir = Input.get_vector("left", "right", "forward", "backward")
+		direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
+		if direction:
+			velocity.x = direction.x * current_speed
+			velocity.z = direction.z * current_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, current_speed)
+			velocity.z = move_toward(velocity.z, 0, current_speed)
+			
+		move_and_slide()
